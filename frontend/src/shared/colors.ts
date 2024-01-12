cbf4fa01fefadaa1e47a0c9b81294a3c0b6063b3
Jannik Streek <jannik.streek@gmail.com>
2024-01-12 17:43:31 +0100

    fix colors

--- frontend/src/shared/colors.ts
+++ frontend/src/shared/colors.ts
@@ -1,7 +1,7 @@
 const themeGreyColors: string[] = ['#080808', '#242424', '#3a3a3a', '#4f4f4f', '#646464']
 const themeMixedColors: string[] = ['#F75C03', '#D90368', '#820263', '#291720', '#04A777']
 
-export const DEFAULT_COLOR = '#666666'
+export const DEFAULT_COLOR = '#9E9E9E'
 
 export enum ColorTheme {
   Mixed,
--- frontend/src/shared/defaultChartSettings.ts
+++ frontend/src/shared/defaultChartSettings.ts
@@ -43,7 +43,16 @@ export const defaultWordchartSettings: ReactWordcloudSettings = {
 }
 
 export const defaultGrammaticalCategoryColors: GrammaticalCategoryColors = {
-  verb: '#A53139', adjective: '#0035DA', noun: '#000000', preposition: '#199463', default: DEFAULT_COLOR, adverb: '#199463', pronoun: '#AD4AC6', article: '#3194E7', conjuncture: '#FFFF42', misc: DEFAULT_COLOR
+  verb: '#E53935',
+  adjective: '#303F9F',
+  noun: '#000000',
+  preposition: '#388E3C',
+  default: DEFAULT_COLOR,
+  adverb: '#F57C00',
+  pronoun: '#7B1FA2',
+  article: '#0288D1',
+  conjuncture: '#EC187A',
+  misc: DEFAULT_COLOR
 }
 
 export const defaultPaginationCount = 21

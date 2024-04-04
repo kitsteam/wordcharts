bb69084208bebf9b14d1fe42f005756f23e4de29
nwittstruck <nwittstruck@users.noreply.github.com>
2024-04-04 17:19:18 +0200

    chore: upgrade react-wordcloud #69: use hash for testing

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,7 @@ config :wordcharts, WordchartsWeb.Endpoint,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.14.29",
+  version: "0.20.2",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),

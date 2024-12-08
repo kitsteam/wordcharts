8491e27518c9632fc1c3916fc8391c50faead63c
Nicholas <nwittstruck@users.noreply.github.com>
2024-12-08 11:02:43 +0100

    chore: bump version to 0.1.11

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wordcharts.MixProject do
   def project do
     [
       app: :wordcharts,
-      version: "0.1.10",
+      version: "0.1.11",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
